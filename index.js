49af52ecc7b284e41184546a192bd1d4d4c2cea7
Maaz Ahmed Khan <amaaz5315@gmail.com>
2024-12-26 13:04:41 +0500

    index.html

---
+++ index.js
@@ -0,0 +1,27 @@
+// Addition
+var num = prompt("Enter Your First Number");
+var num2 = prompt("Enter Your Second Number");
+
+// Strings ko numbers mein convert kar rahe hain
+var total = Number(num) + Number(num2);
+
+alert( + total);
+
+
+// Subraction
+var num = prompt("Enter Your First Number")
+var num2 = prompt("Enter Your Second Number")
+var total = num - num2;
+alert(total);
+
+// Multiplication
+var num = prompt("Enter Your First Number")
+var num2 = prompt("Enter Your Second Number")
+var total = num * num2;
+alert(total);
+
+// Division
+var num = prompt("Enter Your First Number")
+var num2 = prompt("Enter Your Second Number")
+var total = num / num2;
+alert(total);
